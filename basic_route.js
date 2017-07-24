718af0860fa66c2a0fb9f8fca5d722b1b0fdb83f
Jayakrishnan Jayabal <jayakrishnan.jayabal@acquia.com>
2017-07-24 22:48:52 +0530

    Add Routes and Dynamic routes.

---
+++ basic_route.js
@@ -0,0 +1,29 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('You\’re in reception. How can I help you?');
+});
+
+app.get('/basement', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('You\’re in the wine cellar. Those bottles are mine!');
+});
+
+app.get('/floor/numberone/bedroom', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hey, this is a private area!');
+});
+
+app.get('/floor/:floornum/bedroom', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('You\’re in the bedroom on floor n°' + req.params.floornum);
+});
+app.use(function(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(404).send('Page cannot be found!');
+});
+
+app.listen(8080);
